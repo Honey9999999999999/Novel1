3582da731bd6b35a7e2c39d8eef0ff6a91f6b3af
Honey9999999999999 <vsorokin73@gmail.com>
2023-11-16 21:26:36 +0400

    Saves is Done

--- Assets/Project/Resourses/Scripts/PlayerData/PlayerDataSetter.cs
+++ Assets/Project/Resourses/Scripts/PlayerData/PlayerDataSetter.cs
@@ -1,4 +1,5 @@
 ﻿using Fungus;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +8,25 @@ namespace Assets.Project.Resourses.Scripts
 {
     public class PlayerDataSetter : MonoBehaviour
     {
+        [SerializeField] private Character player;
         [SerializeField] private List<Text> playerNames;
-        
 
-        private void Start()
-        {            
-            SetName();
+        private void OnEnable()
+        {
+            SaveManager.OnVarLoaded += SetName;
+        }
+        private void OnDisable()
+        {
+            SaveManager.OnVarLoaded -= SetName;
         }
 
         public void SetName() 
         {
+            if (player == null)
+                throw new Exception("Player is not find");
+
+            player.SetNameText(Progress.instance.save.playerInfo.playerName);
+
             foreach (var playerName in playerNames)
             {
                 playerName.text = Progress.instance.save.playerInfo.playerName;
--- Assets/Project/Resourses/Scripts/PlayerData/SaveManager.cs
+++ Assets/Project/Resourses/Scripts/PlayerData/SaveManager.cs
@@ -1,4 +1,5 @@
 using Fungus;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,7 +9,8 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Button))]
 public class SaveManager : MonoBehaviour
 {
-    [SerializeField] private Flowchart flowchart;
+    public static event Action OnVarLoaded;
+
     private void Start()
     {
         Progress.instance.OnSaveLoaded += CheckSaves;
@@ -30,7 +32,15 @@ public class SaveManager : MonoBehaviour
     }
     public void LoadVariables()
     {
-        flowchart.SetStringVariable("playerName", Progress.instance.save.playerInfo.playerName);
+        Flowchart flowchart = FindObjectOfType<Flowchart>();
+
+        if (flowchart == null)
+            throw new Exception("Flowchart is not find");
+
+        if(Progress.instance.save.playerInfo.playerName.Equals(""))
+            flowchart.SetStringVariable("playerName", "Main Hero");
+        else
+            flowchart.SetStringVariable("playerName", Progress.instance.save.playerInfo.playerName);
 
         flowchart.SetIntegerVariable("C_Level", Progress.instance.save.playerInfo.cSharp);
         flowchart.SetIntegerVariable("Unity_Level", Progress.instance.save.playerInfo.unity);
@@ -39,6 +49,8 @@ public class SaveManager : MonoBehaviour
         flowchart.SetIntegerVariable("Morgan_Friendhsip", Progress.instance.save.playerInfo.morgan);
         //flowchart.SetIntegerVariable("playerName", Progress.instance.save.playerInfo.vondarm);
         flowchart.SetIntegerVariable("Joly_Friendhsip", Progress.instance.save.playerInfo.joly);
+
+        OnVarLoaded?.Invoke();
     }
 
     public void SaveScene() => Progress.instance.save.level = SceneManager.GetActiveScene().buildIndex;
