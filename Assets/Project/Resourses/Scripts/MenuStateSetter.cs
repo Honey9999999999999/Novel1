9480e62671bc4f3229cd2b1e73fcadb27075f102
Honey9999999999999 <vsorokin73@gmail.com>
2023-12-02 21:07:20 +0400

    day4-day5_endings

--- Assets/Project/Resourses/Scripts/MenuStateSetter.cs
+++ Assets/Project/Resourses/Scripts/MenuStateSetter.cs
@@ -19,7 +19,7 @@ public class MenuStateSetter : MonoBehaviour
 
     private void Start()
     {
-        if (Progress.instance.save.level != 0 || !Progress.instance.playerAutorizationState)
+        if (Progress.instance.save.level != 0)
             SetMenuState();
     }
     
